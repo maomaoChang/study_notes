222a2bce66dd6313e4f62c411b0ec4a7493c5c2d
maomaoChang <32168003+maomaoChang@users.noreply.github.com>
2018-06-19 23:58:25 +0800

    add  exception summary

---
+++ Java/ExceptionSummary.java
@@ -0,0 +1,12 @@
+/**
+ * @author maomaochang
+ * 异常处理的优势：将检测错误从处理错误中分离出来
+ * Object<---Throwable接口
+ * 				Error: LinkageError、VirtaulMachineError等      			
+ * 				Exception
+ */
+public class  ExceptionSummary{
+	public static void main(String[] args) {
+		
+	}
+}
