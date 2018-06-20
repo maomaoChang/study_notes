1067f7464ee0dc5b7fc623be19e088a6be2b69a9
maomaoChang <32168003+maomaoChang@users.noreply.github.com>
2018-06-21 06:40:42 +0800

    exception class summary

--- Java/ExceptionSummary.java
+++ Java/ExceptionSummary.java
@@ -15,20 +15,21 @@
  * 2.异常类一般至少有两个构造方法,无参方法和一个带可描述这个异常的String参数构造方法
  * 3.异常的捕获遵循栈规则,先判断异常在哪里被捕获并处理
  * 4.捕获异常时,子类的catch块要写在前面,父类的写在后面,否则会编译失败
+ * 5.继承java.lang.Exception可以自定义异常类
  */
 
 public class  ExceptionSummary{
 	public static void main(String[] args) {
-		//异常类初始化
+		//1.异常类初始化
 		IllegalArgumentException ex1 = new IllegalArgumentException("Wrong Argument"); //
-		//异常信息的获取
+		//2.异常信息的捕获与处理
 		try{
-			System.out.println(sum(new int[]{1,2,3,4,5}));
+			System.out.println(division(10,0));
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
 			System.out.println("\n" + ex.getMessage());
-			System.out.println("\n" + ex.toString());
+			System.out.println("\n" + ex.toString() + "\n");
 
 			System.out.println("从getStackTrace()中获取的信息如下：");
 			StackTraceElement[] traceElements = ex.getStackTrace();
@@ -38,17 +39,65 @@ public class  ExceptionSummary{
 				System.out.println(traceElements[i].getLineNumber() + ")");
 			}
 
-		}		
+		}
+		//3.finally字句一定会执行,即使在到达finally之前有return语句
+		finally{
+			System.out.println("\n" + "finally字句一定会执行");
+		}
+
 	}
 
 
+	//4.无法处理,重新抛出异常。
+	public static double division (double x, double y){
+		double value;
+		try{
+			 value = divide(x,y);	
+		}
+		catch(IllegalArgumentException e){
+			throw new IllegalArgumentException("除数为0！(封装以后重新抛出异常)"); //重新抛出新异常,链式异常(目前未看到效果Page402)
+			//throw e; //重新抛出
+		}
+
+		return value;
+
+	}
+
 	public static int sum(int[] list){
 		int result = 0;
-
 
 		for(int i = 0; i <= list.length; i++){
 			result += list[i];
 		}
 		return result;
 	}
+
+	//声明并抛出异常
+	public static double divide(double x, double y  ) throws IllegalArgumentException{
+		if(y != 0){
+			return x / y;
+		}
+		else{
+			throw new IllegalArgumentException("Zero can not be divided");
+		}
+	}
+
+
+	//5.自定义异常类
+	class InvalidRadiusException extends Exception{
+		private double radius;
+
+		//在初始化圆类时,一旦检测到radius小于0,即可抛出该异常
+		InvalidRadiusException(double radius){
+			super("Invalid radius" + radius);
+			this.radius = radius;
+		}
+
+		double getRadius(){
+			return this.radius;
+		}
+	}
+
+
+
 }
